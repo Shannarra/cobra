5e929bc261e07d8175feadb2b6c466f0aecd112d
Shannarra <petarangelov15@gmail.com>
2020-09-19 19:38:21 +0300

    init 2

---
+++ Cobra/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cobra
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
