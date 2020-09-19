c04496356241a73a0c3e9ec0d6fd9cff7e80c282
Shannarra <petarangelov15@gmail.com>
2020-09-19 21:38:25 +0300

    Added printing for the parsed symbols

--- Cobra/Program.cs
+++ Cobra/Program.cs
@@ -17,7 +17,33 @@ namespace Cobra
 
                 var parser = new Parser(line);
                 var expr = parser.Parse();
+                var color = Console.ForegroundColor;
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                TreePrint(expr);
+                  
+                Console.ForegroundColor = color;
             }
+        }
+
+        static void TreePrint(SyntaxNode node, string spacing = "", bool isLast = true) // printing stolen from windows cmd
+        {
+            var symbol = isLast ? "└──" : "├──";
+            
+            Console.Write($"{spacing}{symbol}{node.Kind}");
+
+            if (node is SyntaxToken token && token.Value != null)
+                Console.Write($"\t{token.Value}");
+
+            Console.WriteLine();
+
+            spacing += isLast ? "\t" : "│\t";
+
+            var lastChild = node.GetChildren().LastOrDefault();
+
+            foreach (var child in node.GetChildren())
+                TreePrint(child, spacing, child == lastChild);
         }
     }
 }
